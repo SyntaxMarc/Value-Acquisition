a50f899d14ef8c15d38174eb53cbcdcb194d58b4
Marc Anthony I. Pala <marcpala9@gmail.com>
2025-09-28 14:38:39 +0800

    Create mock_data.js

---
+++ mock_data.js
@@ -0,0 +1,64 @@
+// mock_data.js - Mock data simulating the API feed for September 28th
+
+let mockGameData = [
+    { 
+        id: 1, 
+        sport: 'MLB', 
+        home: 'Cubs', 
+        away: 'Yankees', 
+        date: '2025-09-28T19:05:00', 
+        oddsHome: 2.10, 
+        oddsAway: 1.80, 
+        probHome: 46, 
+        probAway: 54,
+        lineupStatus: 'Expected'
+    },
+    { 
+        id: 2, 
+        sport: 'MLB', 
+        home: 'Dodgers', 
+        away: 'Giants', 
+        date: '2025-09-28T21:40:00', 
+        oddsHome: 1.65, 
+        oddsAway: 2.35, 
+        probHome: 63, 
+        probAway: 37,
+        lineupStatus: 'Impactful'
+    },
+    { 
+        id: 3, 
+        sport: 'MLB', 
+        home: 'Red Sox', 
+        away: 'Blue Jays', 
+        date: '2025-09-28T18:30:00', 
+        oddsHome: 1.95, 
+        oddsAway: 1.95, 
+        probHome: 52, 
+        probAway: 48,
+        lineupStatus: 'Expected'
+    },
+    { 
+        id: 4, 
+        sport: 'MLBB', 
+        home: 'Blacklist Int.', 
+        away: 'ONIC Esports', 
+        date: '2025-09-29T10:00:00', 
+        oddsHome: 2.50, 
+        oddsAway: 1.50, 
+        probHome: 35, 
+        probAway: 65,
+        lineupStatus: 'Expected'
+    },
+    { 
+        id: 5, 
+        sport: 'MLB', 
+        home: 'Astros', 
+        away: 'Mariners', 
+        date: '2025-09-29T20:00:00', 
+        oddsHome: 2.05, 
+        oddsAway: 1.70, 
+        probHome: 50, 
+        probAway: 50,
+        lineupStatus: 'Expected'
+    }
+];
